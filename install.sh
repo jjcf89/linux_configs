b9eeb56a6d6208980f27d54ccd9689db8495d934
Cormier, Jonathan <jcormier@criticallink.com>
2015-09-28 11:10:09 -0400

    Add script to remap trackball back button to middle click

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo true
 
 cd ~/
 
-HOME_FILES='.bashrc .bashrc_buildenv .bash_aliases .completion .conkyrc .gitconfig .vimrc .astylerc'
+HOME_FILES='.bashrc .bashrc_buildenv .bash_aliases .completion .conkyrc .gitconfig .vimrc .astylerc .xsessionrc'
 
 for f in $HOME_FILES; do
     echo Copying $BASE/$f to .
